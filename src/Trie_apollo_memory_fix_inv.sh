bb12622bc661824b62f20da8984b111a5e28db9e
yunhong <yunhong@tamu.edu>
2016-01-24 19:53:53 -0600

    1st ci

---
+++ src/Trie_apollo_memory_fix_inv.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Trie script
+
+keyFile=300k_rule_subset-evict-noise-withID-
+
+ipfileFolder=~/mapped_caida_trace/
+
+memSize=(120 245 272 300 326 353 380 407 435)
+
+feedbackPortion=(0.01 0.05 0.1 0.2 0.4 0.6)
+
+interval=(2000000 5000000 10000000 20000000 50000000 100000000)
+
+blackKeySize=(500 1000 2000 4000 8000)
+
+count=(0 1 2 3 4 5 6 7 8 9 10)
+
+for i in $(seq 0 4)
+
+do
+	export OMP_NUM_THREADS=8
+
+	./rl_msw ${keyFile} ${memSize[0]} ${ipfileFolder} ${feedbackPortion[2]} ${interval[0]} ${blackKeySize[1]} ${count[i]}
+
+done
+
+
+
+
+
+
